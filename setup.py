6e1486902ba338d8298b86263c38b41901b29974
Alexander Davydov <nyddle@gmail.com>
2014-02-02 01:06:21 +0400

    requirements added, script renamed

--- setup.py
+++ setup.py
@@ -12,5 +12,10 @@ setup(
     description='Save your code snippets in the cloud.',
     long_description=open('README.txt').read(),
     install_requires=[
+        "argparse>=1.2.1",
+        "args>=0.1.0",
+        "clint>=0.3.3",
+        "requests>=2.2.0"
+        "wsgiref>=0.1.2"
     ],
 )
