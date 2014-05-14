b77f4f8414bf02a8549e0f5d820fda04abddea65
Alexander Davydov <nyddle@gmail.com>
2014-05-15 02:32:55 +0400

    version bumped

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='stash',
-    version='0.0.11',
+    version='0.0.12',
     author='Alexander Davydov',
     author_email='nyddle@gmail.com',
     packages=['stash' ],
